cac4079668e0b352d43534a6c5bf3d8a1a602662
Lilian Schall <lilian.schall@epita.fr>
2023-11-21 22:06:51 +0100

    feat: added new index url for core app

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import URLPattern, URLResolver, path
+from typing import List
+
+from . import views
+
+urlpatterns : List[URLPattern | URLResolver] = [
+    path("", views.index, name="index")
+]
