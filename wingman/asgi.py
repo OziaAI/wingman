e36c8ff1771e2aa2ffc1ad88da957ea28b4f3a2b
Lilian Schall <lilian.schall@epita.fr>
2023-11-29 18:10:32 +0100

    fix: fixed origin checker for websocket when a reverse proxy redirects incoming websocket to wingman

--- wingman/asgi.py
+++ wingman/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import OriginValidator
 from django.core.asgi import get_asgi_application
 
 import chat.routing
@@ -24,8 +24,9 @@ django_agsi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_agsi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        "websocket": OriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
+            [ "*" ]
         )
     }
 )
